95a9ecf9b4edcccaccaa002594417501d9a3e3da
Kévin Morpain <kevin.morpain@pretto.fr>
2022-06-16 15:12:03 +0200

    Add Map with points

---
+++ app/javascript/src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Suspense>
+    <MapView />
+  </Suspense>
+</template>
+
+<script setup>
+import MapView from './MapView.vue'
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@500;900&display=swap');
+@import "mapbox-gl/dist/mapbox-gl.css";
+@import "v-mapbox/dist/v-mapbox.css";
+
+html, body {
+  margin: 0;
+  font-family: 'Inter', Arial, sans-serif;
+  font-weight: 500;
+}
+</style>
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root to: 'projects#map'
   get 'map', to: 'projects#map', as: :map
+  get 'map_results', to: 'projects#map_results', as: :map_results
 end
