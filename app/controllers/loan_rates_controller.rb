581550f27f9df710ffe819f62776e7b528a128c5
Ophélie Raymond-Duhamel <ophelieraymond-duhamel@MacBook-Pro-de-Ophelie.local>
2022-06-16 10:09:26 +0200

    Delete unecessary methods

--- app/controllers/loan_rates_controller.rb
+++ app/controllers/loan_rates_controller.rb
@@ -15,44 +15,6 @@ class LoanRatesController < ApplicationController
     end
   end
 
-  # POST /loan_rates or /loan_rates.json
-  def create
-    @loan_rate = LoanRate.new(loan_rate_params)
-
-    respond_to do |format|
-      if @loan_rate.save
-        format.html { redirect_to loan_rate_url(@loan_rate), notice: "Loan rate was successfully created." }
-        format.json { render :show, status: :created, location: @loan_rate }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @loan_rate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /loan_rates/1 or /loan_rates/1.json
-  def update
-    respond_to do |format|
-      if @loan_rate.update(loan_rate_params)
-        format.html { redirect_to loan_rate_url(@loan_rate), notice: "Loan rate was successfully updated." }
-        format.json { render :show, status: :ok, location: @loan_rate }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @loan_rate.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /loan_rates/1 or /loan_rates/1.json
-  def destroy
-    @loan_rate.destroy
-
-    respond_to do |format|
-      format.html { redirect_to loan_rates_url, notice: "Loan rate was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
-
   def load_csv_file(file_path)
     CSV.foreach(file_path, headers: true) do |row|
       # TODO : Convert data into integer, float or other type
@@ -60,15 +22,4 @@ class LoanRatesController < ApplicationController
       LoanRate.create!(row.to_hash)
     end
   end
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_loan_rate
-      @loan_rate = LoanRate.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def loan_rate_params
-      params.require(:loan_rate).permit(:address, :rate)
-    end
 end
