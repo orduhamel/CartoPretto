d78b3a30abd8af0b25cd04e5aea5ece6e27b9d9c
Ophélie Raymond-Duhamel <ophelieraymond-duhamel@MacBook-Pro-de-Ophelie.local>
2022-06-16 14:05:00 +0200

    Delete parsing csv method from Project model

--- app/models/project.rb
+++ app/models/project.rb
@@ -6,12 +6,5 @@ class Project < ApplicationRecord
   validates :rate_obtained, presence: true
 
   after_validation :geocode, if: :will_save_change_to_adresse_complete?
-
-  def self.load_csv_file(file_path)
-    CSV.foreach(file_path, headers: true) do |row|
-      # TODO : Convert data into integer, float or other type
-      # EX : year = row[1].to_i
-      Project.create!(row.to_hash)
-    end
   end
 end
