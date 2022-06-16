4934090401ff98cad1792c0b07b02f5a342c7ed1
Ophélie Raymond-Duhamel <ophelieraymond-duhamel@MacBook-Pro-de-Ophelie.local>
2022-06-16 14:02:31 +0200

    Add Data and seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Parsing csv"
+project_count = 0
+
+CSV.foreach("lib/assets/project_data.csv", headers: true) do |row|
+  next if row[3].nil? || row[6].nil?
+  Project.create!(row.to_hash)
+  project_count += 1
+  puts "Project created ##{project_count}"
+end
+
+puts "Finished parsing"
