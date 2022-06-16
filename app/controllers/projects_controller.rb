0f6ea73635426a7fa5453df2dd41736903caff95
Ophélie Raymond-Duhamel <ophelieraymond-duhamel@MacBook-Pro-de-Ophelie.local>
2022-06-16 14:54:26 +0200

    Add data to makers

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,3 +1,6 @@
+require 'action_view'
+include ActionView::Helpers::NumberHelper
+
 class ProjectsController < ApplicationController
   # GET /projects or /projects.json
   def index
@@ -11,7 +14,14 @@ class ProjectsController < ApplicationController
     @markers = @projects.geocoded.map do |project|
       {
         lat: project.latitude,
-        lng: project.longitude
+        lng: project.longitude,
+        address: project.adresse_complete,
+        rate: project.rate_obtained,
+        duration: project.duration,
+        loan_amount: project.loan_amount.round(-1),
+        nb_mortgagors: project.nb_mortgagors,
+        total_income: number_to_human(project.total_income, :format => '%n%u', :units => { :thousand => 'K' }),
+        age: project.age_moyen
       }
     end
   end
